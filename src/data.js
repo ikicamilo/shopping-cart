ed9776e3c954df018069bdffca21e34cd6a8fb7d
ikicamilo <ikicamilo@gmail.com>
2023-05-28 16:23:54 +0100

    First commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Samsung_Galaxy_S7_and_S7_Edge.png/375px-Samsung_Galaxy_S7_and_S7_Edge.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://lh3.googleusercontent.com/8BorfTKuWHHHFyKn8v5xoA2irUBnYWu35YlspTD-cJrPHd7JAcPo1_MGgcjkRGKT6grmFV3jOULukfNlvifjWhcViIreteX7lQA=rw-e365-w525",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://cdn.shopify.com/s/files/1/0042/7628/9571/products/Note12Plus.jpg?v=1682358953&width=750",
+    amount: 1,
+  },
+];
